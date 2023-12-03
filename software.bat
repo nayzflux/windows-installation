281eb08be14de09c028b4adb790310c0af0f43d0
NayZ <55506519+nayzflux@users.noreply.github.com>
2023-12-03 20:44:27 +0100

    Update software.bat
    ✨ Improving softwares list

--- software.bat
+++ software.bat
@@ -1,11 +1,25 @@
+# Launcher
 winget install EpicGames.EpicGamesLauncher
 winget install Ubisoft.Connect
-winget install Ubisoft.Connect
-winget install OBSProject.OBSStudio
-winget install Discord.Discord
-winget install Spotify.Spotify
-winget install Mozilla.Firefox
-winget install VB-Audio.Voicemeeter.Banana
 winget install ElectronicArts.EADesktop
-winget install Microsoft.VisualStudioCode
 winget install Valve.Steam
+
+# Streaming
+winget install OBSProject.OBSStudio
+
+# Tools
+winget install VB-Audio.Voicemeeter.Banana
+winget install Elgato.StreamDeck
+
+# Communication
+winget install Discord.Discord
+
+# Multimedia
+winget install Spotify.Spotify
+winget install VideoLAN.VLC
+winget install Mozilla.Firefox
+
+# Developper
+winget install Microsoft.VisualStudioCode
+winget install OpenJS.NodeJS.LTS
+winget install Python.Python.3.12
